d8b2b3308fa971bb4bb70d670198eb1d021700b3
DEEPAK <deepak.rockstar1231@gmail.com>
2017-07-22 13:52:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/deepak/loginvalid/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.deepak.loginvalid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, loginscreen.class);
+        startActivity(intent);
+    }
+}
